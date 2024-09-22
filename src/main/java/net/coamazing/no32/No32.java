e206516b0fca953d86e0b809368b0289213b3b45
CoAmazing <116113452+CoADontAmazing@users.noreply.github.com>
2024-09-22 21:04:13 +0300

    Add files via upload

---
+++ src/main/java/net/coamazing/no32/No32.java
@@ -0,0 +1,9 @@
+package net.coamazing.no32;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class No32 implements ClientModInitializer {
+	public static final String VERSION = "1", ID = "no32", NAME = "No32";	
+	@Override
+	public void onInitializeClient() {}
+}
