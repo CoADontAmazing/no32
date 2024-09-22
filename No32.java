bcc34da9f5419abce28ebca2158c538e8a42e4c6
CoAmazing <116113452+CoADontAmazing@users.noreply.github.com>
2024-09-22 21:01:28 +0300

    Delete No32.java

--- No32.java
+++
@@ -1,9 +0,0 @@
-package net.coamazing.no32;
-
-import net.fabricmc.api.ClientModInitializer;
-
-public class No32 implements ClientModInitializer {
-	public static final String VERSION = "1", ID = "no32", NAME = "No32";	
-	@Override
-	public void onInitializeClient() {}
-}
