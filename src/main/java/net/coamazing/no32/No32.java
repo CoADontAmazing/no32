2ad3ac485a9a2469e33abc478c8e869b5a6d0b8c
CoAmazing <116113452+CoADontAmazing@users.noreply.github.com>
2024-09-23 09:33:57 +0300

    Update No32.java

--- src/main/java/net/coamazing/no32/No32.java
+++ src/main/java/net/coamazing/no32/No32.java
@@ -4,6 +4,5 @@ import net.fabricmc.api.ClientModInitializer;
 
 public class No32 implements ClientModInitializer {
 	public static final String VERSION = "1", ID = "no32", NAME = "No32";	
-	@Override
-	public void onInitializeClient() {}
+	@Override public void onInitializeClient() {}
 }
